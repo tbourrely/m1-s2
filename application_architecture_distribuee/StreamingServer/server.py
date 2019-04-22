42b530bd773498d6d3991871289e7ac117c55b4f
thomas <thomas@ubuntu.numericable.fr>
2019-04-22 17:33:58 +0200

    uncomment flask streaming

--- application_architecture_distribuee/StreamingServer/server.py
+++ application_architecture_distribuee/StreamingServer/server.py
@@ -1,49 +1,47 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import os
 from pathlib import Path  # python3 only
 from dotenv import load_dotenv
+from controllers.utils import doesFileExists
+from database.Client import Client
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-import controllers.tokens as tokensController
-import controllers.apiKey as apiKeyController
-
 app = Flask(__name__)
 
+def streamFile(fileName):
+    path = './' + os.getenv('FILES_DIR') + '/' + fileName
+    with open(path, 'rb') as r:
+        data = r.read(1024)
+        while data:
+            yield data
+            data = r.read(1024)
 
-# def streamFile(fileName):
-#     path = './' + FILES_DIR + '/' + fileName
-#     with open(path, 'rb') as r:
-#         data = r.read(1024)
-#         while data:
-#             yield data
-#             data = r.read(1024)
+        r.close()
 
-#         r.close()
+@app.route("/stream/<string:filename>", methods=['GET'])
+def stream(filename):
+    fileExists = doesFileExists(filename)
 
-# @app.route("/stream/<string:filename>", methods=['GET'])
-# def stream(filename):
-#     fileExists = doesFileExists(filename)
-
-#     if not fileExists:
-#         return Response(status=404)
+    if not fileExists:
+        return Response(status=404)
     
-#     token = request.args.get('token')
+    token = request.args.get('token')
 
-#     if token is None or 0 == len(token):
-#         return Response(status=401)
+    if token is None or 0 == len(token):
+        return Response(status=401)
 
-#     tokenFromDB = security_db.tokens.find_one({'token' : token, 'file': filename})
+    tokenFromDB = Client().security.tokens.find_one({'token' : token, 'file': filename})
 
-#     if tokenFromDB is None:
-#         return Response(status=401)
+    if tokenFromDB is None:
+        return Response(status=401)
 
-#     print(tokenFromDB)
+    print(tokenFromDB)
     
-#     # TODO
-#     # Return stream + delete token
-#     # rv = Response(streamFile(filename), mimetype="audio/aac")
-    
-#     return Response('token : ' + token)
+    # TODO
+    # delete token ?
+    return Response(streamFile(filename), mimetype="audio/aac")
 
+if __name__ == '__main__':
+    app.run(debug=True)
